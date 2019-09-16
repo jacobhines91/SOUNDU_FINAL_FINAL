c67b3ccb09ced434ef352b7718223b5e78180103
Jacob Hines <jacob_hines@hotmail.com>
2019-09-16 15:59:11 -0500

    updated html to match example

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -17,32 +17,32 @@
     $("#bkgOverlay").fadeOut(400);
     $("#delayedPopup").fadeOut(300);
   } 
+  
+  // var tag = document.createElement('script');
 
-  var tag = document.createElement('script');
+  // tag.src = "https://www.youtube.com/iframe_api";
+  // var firstScriptTag = document.getElementsByTagName('script')[0];
+  // firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-  tag.src = "https://www.youtube.com/iframe_api";
-  var firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  // // 3. This function creates an <iframe> (and YouTube player)
+  // //    after the API code downloads.
+  // var player;
+  // function onYouTubeIframeAPIReady() {
+  //   player = new YT.Player('player', {
+  //     height: '390',
+  //     width: '720',
+  //     videoId: 'M7lc1UVf-VE',
+  //     events: {
+  //       'onReady': onPlayerReady,
+  //       'onStateChange': onPlayerStateChange
+  //     }
+  //   });
+  // }
 
-  // 3. This function creates an <iframe> (and YouTube player)
-  //    after the API code downloads.
-  var player;
-  function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-      height: '390',
-      width: '720',
-      videoId: 'M7lc1UVf-VE',
-      events: {
-        'onReady': onPlayerReady,
-        'onStateChange': onPlayerStateChange
-      }
-    });
-  }
-
-  // 4. The API will call this function when the video player is ready.
-  function onPlayerReady(event) {
-    event.target.playVideo();
-  }
+  // // 4. The API will call this function when the video player is ready.
+  // function onPlayerReady(event) {
+  //   event.target.playVideo();
+  // }
 
   // 5. The API calls this function when the player's state changes.
   //    The function indicates that when playing a video (state=1),
